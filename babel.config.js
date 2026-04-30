0dff4ed0d83782c84c3a7483c751e421de8ac80c
Rudra <buddhuprogrammer@gmail.com>
2026-04-30 14:07:13 +0530

    fix deps with clerk and sentry

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+    ],
+    plugins: [
+      "nativewind/babel",
+      "react-native-reanimated/plugin" // ⚠️ MUST be last
+    ],
   };
 };
