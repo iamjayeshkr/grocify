d9e5a2d5695f8f9e62870778e217efa6ee0196a7
Rudra <buddhuprogrammer@gmail.com>
2026-04-30 15:43:21 +0530

    fix metro sentry + nativewind

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,13 @@
+const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getSentryExpoConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+// 👉 apply NativeWind FIRST
+const nativeWindConfig = withNativeWind(config, {
+  input: "./global.css",
+});
+
+// 👉 then merge Sentry config correctly
+module.exports = getSentryExpoConfig(__dirname, nativeWindConfig);
